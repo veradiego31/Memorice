c790f150e924fa735b668eb0033a989c616fb5da
davera <davera@roshka.com>
2022-02-21 19:51:24 -0500

    first commit

---
+++ Memorice/MemoriceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriceApp.swift
+//  Memorice
+//
+//  Created by Diego Vera on 2022-02-21.
+//
+
+import SwiftUI
+
+@main
+struct MemoriceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
